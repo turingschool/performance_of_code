2d87712b91b563ef67901fd4095a2beff94eda0b
worace <horace.d.williams@gmail.com>
2015-05-04 23:35:14 -0600

    cleanup

--- fib.rb
+++ fib.rb
@@ -1,8 +1,3 @@
-# ==== Fibonacci Sequence ====
-# Install ruby-prof gem (gem install ruby-prof)
-# Then profile this file with:
-# $ ruby-prof performance-of-code.rb
-
 def fib(n)
   return n if (0..1).include? n
   fib(n-1) + fib(n-2) if n > 1
